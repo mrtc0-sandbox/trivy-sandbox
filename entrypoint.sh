da960cd0af2ce3bb7c9162c5b4cf38194b652f0a
Kohei Morita <mrtc0@ssrf.in>
2022-06-01 12:55:18 +0900

    chore: debug
    Signed-off-by: Kohei Morita <mrtc0@ssrf.in>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-trivy -q conf -f sarif  --policy policies/policy --namespaces user manifests/ \
+set -ex
+
+trivy -q conf -f sarif --policy policies/policy --namespaces user manifests/ \
   | jq -r '.runs[].results[] | "\(.level[0:1]):\("manifests/" + .locations[].physicalLocation.artifactLocation.uri):\(.locations[].physicalLocation.region.endLine) \(.message.text)"' \
   | reviewdog -efm="%t%f:%l %m" --diff="git diff ${GITHUB_REF}" -reporter=github-pr-review
