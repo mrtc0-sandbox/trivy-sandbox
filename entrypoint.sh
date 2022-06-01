dbdd12b369590fe53fba447f407ef715d45482fc
Kohei Morita <mrtc0@ssrf.in>
2022-06-01 12:35:10 +0900

    feat: set reporter option
    Signed-off-by: Kohei Morita <mrtc0@ssrf.in>

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 trivy -q conf -f sarif  --policy policies/policy --namespaces user manifests/ \
   | jq -r '.runs[].results[] | "\(.level[0:1]):\("manifests/" + .locations[].physicalLocation.artifactLocation.uri):\(.locations[].physicalLocation.region.endLine) \(.message.text)"' \
-  | reviewdog -efm="%t%f:%l %m" --diff="git diff master" -filter-mode=nofilter -tee
+  | reviewdog -efm="%t%f:%l %m" --diff="git diff ${GITHUB_REF}" -reporter=github-pr-review
