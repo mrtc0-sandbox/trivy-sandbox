91ad006562a76307af3dd30fbcfb9b7b621b9c97
Kohei Morita <mrtc0@ssrf.in>
2022-06-01 14:18:38 +0900

    fix: |
    Signed-off-by: Kohei Morita <mrtc0@ssrf.in>

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ set -ex
 
 trivy -q conf -f sarif --policy policies/policy --namespaces user manifests/ \
   | jq '.runs[].results[] | "\(.level[0:1]):\("manifests/" + .locations[].physicalLocation.artifactLocation.uri):\(.locations[].physicalLocation.region.endLine) \(.message.text)"' \
-  | sed "s/\\\\n/<br>/g"
+  | sed "s/\\\\n/<br>/g" \
   | reviewdog -efm="\"%t:%f:%l %m\"" --diff="git diff ${GITHUB_REF}" -reporter=github-pr-review
