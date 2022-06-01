c6fab3cf01c652bfda3adfb970a0a799925702ec
Kohei Morita <mrtc0@ssrf.in>
2022-06-01 13:12:44 +0900

    fix: option
    Signed-off-by: Kohei Morita <mrtc0@ssrf.in>

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@
 set -ex
 
 trivy -q conf -f sarif --policy policies/policy --namespaces user manifests/ \
-  | jq -r '.runs[].results[] | "\(.level[0:1]):\("manifests/" + .locations[].physicalLocation.artifactLocation.uri):\(.locations[].physicalLocation.region.endLine) \(.message.text)"' \
-  | reviewdog -efm="%t%f:%l %m" --diff="git diff ${GITHUB_REF}" -reporter=github-pr-review
+  | jq '.runs[].results[] | "\(.level[0:1]):\("manifests/" + .locations[].physicalLocation.artifactLocation.uri):\(.locations[].physicalLocation.region.endLine) \(.message.text)"' \
+  | reviewdog -efm="\"%t:%f:%l %m\"" --diff="git diff ${GITHUB_REF}" -reporter=github-pr-review
